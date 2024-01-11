0785fb21671bfd7039896efd4eb9114a054c5059
phuongtrieu97coder <phuongcoderinfinity@gmail.com>
2024-01-11 15:28:56 -0800

    cmit_01_11_2024_3:28:PM

--- Codecademy/Express_Middleware_Code_Challenges/src/app.js
+++ Codecademy/Express_Middleware_Code_Challenges/src/app.js
@@ -1,3 +1,3 @@
 //Syntaxes are from Readme.md file
-//This project writes syntaxes into different app.js
+//This project writes syntaxes into different app.js file
 //That's why I don't provide those syntaxes here
